a1b29632008773a62874949dfb7be46678ff0ae1
Doctor Cobweb <spinninghalf@gmail.com>
2014-03-25 16:48:23 +1100

    Initial Commit

---
+++ MyStore_take2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyStore_take2
+//
+//  Created by andre on 25/03/2014.
+//  Copyright (c) 2014 andre trosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MyStore_take2Tests/MyStore_take2Tests.m
@@ -0,0 +1,34 @@
+//
+//  MyStore_take2Tests.m
+//  MyStore_take2Tests
+//
+//  Created by andre on 25/03/2014.
+//  Copyright (c) 2014 andre trosky. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyStore_take2Tests : XCTestCase
+
+@end
+
+@implementation MyStore_take2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
